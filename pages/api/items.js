f4fd2b27708bfd4ef018cc833e2586254d644e49
Nick Allen <nick@nicholasdallen.com>
2020-10-17 09:15:00 -0700

    work on loading states

--- pages/api/items.js
+++ pages/api/items.js
@@ -1,4 +1,7 @@
-export default function handler(req, res) {
+import { sleep } from "../../util/sleep"
+
+export default async function handler(req, res) {
+    await sleep(3);
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify([1,2,3]))
