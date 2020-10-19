000316af85fb9acf11a8bdba1e38b75cbcaf9a64
Nick Allen <nick@nicholasdallen.com>
2020-10-19 16:12:30 -0700

    bg color

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
+import theme from './theme';
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
       const initialProps = await Document.getInitialProps(ctx)
       return { ...initialProps }
     }
-  
+
     render() {
       return (
         <Html>
